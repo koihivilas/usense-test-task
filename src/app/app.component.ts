fa66588a509e13709b8922aca4c8682239e0cfe9
Kyryl <36502485+koihivilas@users.noreply.github.com>
2023-07-23 21:54:46 +0300

    feat: create password strength tester

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'usense-test-task';
+  title = 'usense-test-task'
+  password: string = ''
+  strength: string = ''
+  colors: string[] = ['gray', 'gray', 'gray']
+
+  onInput(event: any) {
+    this.password = event.target.value
+    this.checkPasswordStrength()
+    this.updateColors()
+  }
+
+  checkPasswordStrength() {
+    let hasLetters = /[a-zA-Z]/.test(this.password)
+    let hasDigits = /\d/.test(this.password)
+    let hasSymbols = /\W/.test(this.password)
+
+    if (this.password.length == 0) {
+      this.strength = ''
+    } else if (this.password.length < 8) {
+      this.strength = 'short'
+    } else if (hasLetters && hasDigits && hasSymbols) {
+      this.strength = 'strong'
+    } else if ((hasLetters && hasSymbols) || (hasLetters && hasDigits) || (hasDigits && hasSymbols)) {
+      this.strength = 'medium'
+    } else {
+      this.strength = 'easy'
+    }
+  }
+
+  updateColors() {
+    switch (this.strength) {
+      case 'short':
+        this.colors = ['red', 'red', 'red']
+        break;
+      case 'easy':
+        this.colors = ['red', 'gray', 'gray']
+        break;
+      case 'medium':
+        this.colors = ['yellow', 'yellow', 'gray']
+        break;
+      case 'strong':
+        this.colors = ['green', 'green', 'green']
+        break;
+      default:
+        this.colors = ['gray', 'gray', 'gray']
+        break;
+    }
+  }
 }
